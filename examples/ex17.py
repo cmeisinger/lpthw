a7ab69a0e0d2698591cbdd04956d5b1b7bedd35a
Chris Meisinger <chris.meisinger@centro.net>
2016-08-04 15:49:11 -0500

    adding in current status

--- examples/ex17.py
+++ examples/ex17.py
@@ -1,11 +1,11 @@
-#1/usr/bin/python
+#!/usr/bin/python
 
 from sys import argv
 from os.path import exists
 
 script, from_file, to_file = argv
 
-#we could do these two on oneline, how?
+#we could do these two on one line, how?
 in_file = open(from_file)
 indata = in_file.read()
 
@@ -16,4 +16,5 @@ print "Alright, all done."
 
 out_file.close()
 in_file.close()
+
 
